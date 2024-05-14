aaf989c9ec7889173a4ff9e0fed2f05d15e4d09a
Jonntz <monteirojonatas23@gmail.com>
2024-05-13 23:19:32 -0300

    Desafio concluido: Abstração de um smartphone com POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("11928285656", "Nokia tijolão", "123435678", 8);
+Iphone iphone = new Iphone("11928285656", "Iphone 15", "12345678", 12);
+
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("Instagram");
